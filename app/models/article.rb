14a2f7c8297a2744b19aae1d044d83c6ee8a8857
Andrew Garcia <andrew.g@truhearing.com>
2024-01-10 13:15:02 -0700

    rails project completion

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, length: { maximum: 40 }
+  validates :title,
+            :content,
+            :author,
+            :category,
+            :published_at, presence: true
 end
